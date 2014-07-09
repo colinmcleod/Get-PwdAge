819ec0f503f7acfebd33ec03baf76f02c933a3c4
Colin <me@colinmcleod.ca>
2014-07-09 13:19:08 -0600

    Update Get-PwdAge.ps1

--- Get-PwdAge.ps1
+++ Get-PwdAge.ps1
@@ -1,5 +1,5 @@
-function Get-PwdAge { [CmdletBinding()] Param ([Parameter(Mandatory=$false, Position=1, ValueFromPipeline=$false, ValueFromPipelineByPropertyName=$false)][String]$Usr)
+function Get-PwdAge { [CmdletBinding()] Param ([Parameter(Mandatory=$false, Position=1, ValueFromPipeline=$false, ValueFromPipelineByPropertyName=$false)][String]$Username)
 
-Get-ADUser -Filter {samaccountname -like $Usr} -Properties pwdlastset | select @{Name="Employee Name";expression={$_.Name}},@{Name="Login";expression={$_.SamAccountName}},@{Name="Age In Days";Expression={(New-TimeSpan ([DateTime]::FromFileTime($_.pwdlastset))).Days}},@{Name="Last Set";Expression={[DateTime]::FromFileTime($_.pwdlastset)}} | ft
+Get-ADUser -Filter {samaccountname -like $Username -and enabled -eq "True"} -Properties pwdlastset | select @{Name="Employee Name";expression={$_.Name}},@{Name="Login";expression={$_.SamAccountName}},@{Name="Age In Days";Expression={(New-TimeSpan ([DateTime]::FromFileTime($_.pwdlastset))).Days}},@{Name="Last Set";Expression={[DateTime]::FromFileTime($_.pwdlastset)}} | ft
 
 }
