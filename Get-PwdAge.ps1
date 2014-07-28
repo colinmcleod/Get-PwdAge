8f9aed61cb30e7b9ab43329bc44329c0a153a0bd
colinmcleod <colin.mcleod@cnty.com>
2014-07-28 12:21:30 -0600

    Set Usr string as Mandatory

--- Get-PwdAge.ps1
+++ Get-PwdAge.ps1
@@ -1,4 +1,4 @@
-function Get-PwdAge { [CmdletBinding()] Param ([Parameter(Mandatory=$false, Position=1, ValueFromPipeline=$false, ValueFromPipelineByPropertyName=$false)][String]$Username)
+function Get-PwdAge { [CmdletBinding()] Param ([Parameter(Mandatory=$true, Position=1, ValueFromPipeline=$false, ValueFromPipelineByPropertyName=$false)][String]$Username)
 
 Get-ADUser -Filter {samaccountname -like $Username -and enabled -eq "True" -and Name -notlike "Administrator"} -Properties pwdlastset | select @{Name="Employee Name";expression={$_.Name}},@{Name="Login";expression={$_.SamAccountName}},@{Name="Age In Days";Expression={(New-TimeSpan ([DateTime]::FromFileTime($_.pwdlastset))).Days}},@{Name="Last Set";Expression={[DateTime]::FromFileTime($_.pwdlastset)}} | Sort-Object -Property "Employee Name","Login","Age In Days","Last Set" | Format-Table -Property "Employee Name","Login","Age In Days","Last Set"
 }
