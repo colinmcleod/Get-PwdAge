85d285d9674ccda5df831efabd6564d8836c8aff
Colin <me@colinmcleod.ca>
2016-03-08 02:26:46 -0700

    Update Get-PwdAge.ps1

--- Get-PwdAge.ps1
+++ Get-PwdAge.ps1
@@ -52,16 +52,16 @@ function Get-PwdAge {
 	$logStamp = Get-Date -Format 'mm.dd.yyyy hh:mm:ss tt'
 
 # --- Error & Log Handling --- #
-	
-    function IsNull($object) {
 
-    if (!$object) {
-        return $true
-    }
+function IsNull($object) {
 
-    if ($object -is [String] -and $object -eq [String]::Empty) {
-        return $true
-    }
+  if (!$object) {
+    return $true
+   }
+
+  if ($object -is [String] -and $object -eq [String]::Empty) {
+     return $true
+   }
 
     if ($object -is [DBNull] -or $object -is [System.Management.Automation.Language.NullString]) {
         return $true
@@ -130,26 +130,7 @@ function sendMail
 	}
 	
 Try {
-Get-ADUser -Filter {
-    samaccountname -like $Username
-    -and enabled -eq "True"
-    -and Name -notlike "Guest"
-    } `
-`
-    -Properties pwdlastset `
-`
-     | select @{
-            Name="Full Name";expression={$_.Name}
-        },@{
-            Name="Login";expression={$_.SamAccountName}
-        },@{
-            Name="Age In Days";Expression={(New-TimeSpan ([DateTime]::FromFileTime($_.pwdlastset))).Days}
-        },@{
-            Name="Last Set";Expression={[DateTime]::FromFileTime($_.pwdlastset)}
-        } `
-`
-| Sort-Object  -Property "Full Name" `
-| Format-Table -Property "Full Name","Login","Age In Days","Last Set"
+Get-ADUser -Filter { samaccountname -like $Username -and enabled -eq "True" -and Name -notlike "Guest" } -Property pwdlastset | select @{ Name="Full Name";expression={$_.Name} },@{ Name="Login";expression={$_.SamAccountName} },@{ Name="Age In Days";Expression={(New-TimeSpan ([DateTime]::FromFileTime($_.pwdlastset))).Days} },@{ Name="Last Set";Expression={[DateTime]::FromFileTime($_.pwdlastset)} } | Sort-Object -Property "Full Name" | Format-Table -Property "Full Name","Login","Age In Days","Last Set"
 }
 Catch {
     
